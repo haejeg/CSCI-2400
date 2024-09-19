d0fd1515f33f3dd439e3a4bb0eb2a5a8374496aa
haejeg <88898962+haejeg@users.noreply.github.com>
2024-09-19 03:04:10 -0600

    Finished version of datalab, no ec.

---
+++ datalab_bits.c
@@ -0,0 +1,430 @@
+/* 
+ * CS:APP Data Lab 
+ * 
+ * <Please put your name and userid here>
+ * 
+ * bits.c - Source file with your solutions to the Lab.
+ *          This is the file you will hand in to your instructor.
+ *
+ * WARNING: Do not include the <stdio.h> header; it confuses the dlc
+ * compiler. You can still use printf for debugging without including
+ * <stdio.h>, although you might get a compiler warning. In general,
+ * it's not good practice to ignore compiler warnings, but in this
+ * case it's OK.  
+ */
+
+#if 0
+/*
+ * Instructions to Students:
+ *
+ * STEP 1: Read the following instructions carefully.
+ */
+
+You will provide your solution to the Data Lab by
+editing the collection of functions in this source file.
+
+INTEGER CODING RULES:
+ 
+  Replace the "return" statement in each function with one
+  or more lines of C code that implements the function. Your code 
+  must conform to the following style:
+ 
+  int Funct(arg1, arg2, ...) {
+      /* brief description of how your implementation works */
+      int var1 = Expr1;
+      ...
+      int varM = ExprM;
+
+      varJ = ExprJ;
+      ...
+      varN = ExprN;
+      return ExprR;
+  }
+
+  Each "Expr" is an expression using ONLY the following:
+  1. Integer constants 0 through 255 (0xFF), inclusive. You are
+      not allowed to use big constants such as 0xffffffff.
+  2. Function arguments and local variables (no global variables).
+  3. Unary integer operations ! ~
+  4. Binary integer operations & ^ | + << >>
+    
+  Some of the problems restrict the set of allowed operators even further.
+  Each "Expr" may consist of multiple operators. You are not restricted to
+  one operator per line.
+
+  You are expressly forbidden to:
+  1. Use any control constructs such as if, do, while, for, switch, etc.
+  2. Define or use any macros.
+  3. Define any additional functions in this file.
+  4. Call any functions.
+  5. Use any other operations, such as &&, ||, -, or ?:
+  6. Use any form of casting.
+  7. Use any data type other than int.  This implies that you
+     cannot use arrays, structs, or unions.
+
+ 
+  You may assume that your machine:
+  1. Uses 2s complement, 32-bit representations of integers.
+  2. Performs right shifts arithmetically and a negative shift value results in zero.
+  3. Has unpredictable behavior when shifting an integer by more
+     than the word size.
+
+EXAMPLES OF ACCEPTABLE CODING STYLE:
+  /*
+   * pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
+   */
+  int pow2plus1(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     return (1 << x) + 1;
+  }
+
+  /*
+   * pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
+   */
+  int pow2plus4(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     int result = (1 << x);
+     result += 4;
+     return result;
+  }
+
+FLOATING POINT CODING RULES
+
+For the problems that require you to implent floating-point operations,
+the coding rules are less strict.  You are allowed to use looping and
+conditional control.  You are allowed to use both ints and unsigneds.
+You can use arbitrary integer and unsigned constants.
+
+You are expressly forbidden to:
+  1. Define or use any macros.
+  2. Define any additional functions in this file.
+  3. Call any functions.
+  4. Use any form of casting.
+  5. Use any data type other than int or unsigned.  This means that you
+     cannot use arrays, structs, or unions.
+  6. Use any floating point data types, operations, or constants.
+
+
+ * STEP 2: Modify the following functions according the coding rules.
+ * 
+ *   IMPORTANT. TO AVOID GRADING SURPRISES, FOLLOW THE DIRECTIONS in README.md
+ */
+
+
+#endif
+/* Copyright (C) 1991-2022 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* glibc's intent is to support the IEC 559 math functionality, real
+   and complex.  If the GCC (4.9 and later) predefined macros
+   specifying compiler intent are available, use them to determine
+   whether the overall intent is to support these features; otherwise,
+   presume an older compiler has intent to support these features and
+   define these macros by default.  */
+/* wchar_t uses Unicode 10.0.0.  Version 10.0 of the Unicode Standard is
+   synchronized with ISO/IEC 10646:2017, fifth edition, plus
+   the following additions from Amendment 1 to the fifth edition:
+   - 56 emoji characters
+   - 285 hentaigana
+   - 3 additional Zanabazar Square characters */
+/* 
+ * bitAnd - x&y using only ~ and | 
+ *   Example: bitAnd(6, 5) = 4
+ *   Legal ops: ~ |
+ *   Max ops: 8
+ *   Rating: 1
+ */
+int bitAnd(int x, int y) {
+  return ~(~x | ~y);
+}
+  /* 
+  * fitsShort - return 1 if x can be represented as a 
+  *   16-bit, two's complement integer.  
+  *   Examples: fitsShort(33000) = 0, fitsShort(-32768) = 1
+  *   Legal ops: ! ~ & ^ | + << >>
+  *   Max ops: 8
+  *   Rating: 1
+  * 
+  * Because of different bit counts, this works (32bit for int and 16bit for short)
+  */
+int fitsShort(int x) {
+  return !((x << 16) >> 16 ^ x);
+} 
+/* 
+ * thirdBits - return word with every third bit (starting from the LSB) set to 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 8
+ *   Rating: 1
+ *   1001 0010 0100 1001
+ */
+int thirdBits(void) {
+  int hex = 0x49;
+  hex = hex << 9 | hex;
+  hex = hex << 18 | hex;
+  return hex;
+}
+
+/* 
+ * anyEvenBit - return 1 if any even-numbered bit in word set to 1
+ *   Examples anyEvenBit(0xA) = 0, anyEvenBit(0xE) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 12
+ *   Rating: 2
+ */
+int anyEvenBit(int x) {
+  int hex = 0x55;
+  hex = hex << 8 | hex;
+  hex = hex << 16 | hex;
+  hex = hex << 24 | hex;
+  return !(!(hex & x));
+}
+/* 
+ * copyLSB - set all bits of result to least significant bit of x
+ *   Example: copyLSB(5) = 0xFFFFFFFF, copyLSB(6) = 0x00000000
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 5
+ *   Rating: 2
+ */
+int copyLSB(int x) {
+  int one = 0xF;
+  x = x & one;
+  x = x<<31;
+  x = x>>31;
+  return x;
+}
+/* 
+ * implication - return x -> y in propositional logic - 0 for false, 1
+ * for true
+ *   Example: implication(1,1) = 1
+ *            implication(1,0) = 0
+ *   Legal ops: ! ~ ^ |
+ *   Max ops: 5
+ *   Rating: 2
+ */
+int implication(int x, int y) {
+    return !(!(!x | y));
+}
+/*
+ * bitCount - returns count of number of 1's in word
+ *   Examples: bitCount(5) = 2, bitCount(7) = 3
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 40
+ *   Rating: 4
+ */
+int bitCount(int x) {
+    int mask = 0x11;
+    int result;
+
+    mask = (mask << 8) | mask;
+    mask = (mask << 16) | mask;
+
+    result = x & mask;
+    x = x >> 1;   
+    result = result + (x & mask); 
+    x = x >> 1; 
+    result = result + (x & mask);
+    x = x >> 1;
+    result = result + (x & mask);
+
+    result = (result & 0xF) + ((result >> 4) & 0xF) + ((result >> 8) & 0xF) + ((result >> 12) & 0xF) + ((result >> 16) & 0xF) + ((result >> 20) & 0xF) + ((result >> 24) & 0xF) + ((result >> 28) & 0xF);
+
+    return result;
+}
+
+
+  // int newSep1 = sepMask1 & result;
+  // result = result >> 4;
+  // int newSep2 = sepMask1 & result;
+  // result = result >> 4;
+  // int newSep3 = sepMask1 & result;
+  // result = result >> 4;
+  // int newSep4 = sepMask1 & result;
+  // return newSep1 + newSep2 + newSep3 + newSep4;
+/*
+ * ezThreeFourths - multiplies by 3/4 rounding toward 0,
+ *   Should exactly duplicate effect of C expression (x*3/4),
+ *   including overflow behavior.
+ *   Examples: ezThreeFourths(11) = 8
+ *             ezThreeFourths(-9) = -6
+ *             ezThreeFourths(1073741824) = -268435456 (overflow)
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 12
+ *   Rating: 3
+ */
+int ezThreeFourths(int x) {
+  int mult = x + x + x; // x*3
+  // if x is a signed bit, add 3 before division
+  int y = (mult >> 31) & 0x3; // bias that is either 0 or 3
+  int z = (mult + y) >>2; // x/4
+  return z;
+}
+/*
+ * satMul3 - multiplies by 3, saturating to Tmin or Tmax if overflow
+ *  Examples: satMul3(0x10000000) = 0x30000000
+ *            satMul3(0x30000000) = 0x7FFFFFFF (Saturate to TMax)
+ *            satMul3(0x70000000) = 0x7FFFFFFF (Saturate to TMax)
+ *            satMul3(0xD0000000) = 0x80000000 (Saturate to TMin)
+ *            satMul3(0xA0000000) = 0x80000000 (Saturate to TMin)
+ *  Legal ops: ! ~ & ^ | + << >>
+ *  Max ops: 25
+ *  Rating: 3
+ */
+int satMul3(int x) {
+    int mult = x + x + x;
+    int overflow = ((x << 1 ^ x) | (mult ^ x)) >> 31;
+    int tmax = ~(1 << 31);
+    int sign = x >> 31;
+
+    int result = (~overflow & mult) |  (overflow & (tmax ^ sign));
+    return result;
+}
+/*
+ * bitParity - returns 1 if x contains an odd number of 0's
+ *   Examples: bitParity(5) = 0, bitParity(7) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 20
+ *   Rating: 4
+ */
+int bitParity(int x) {
+  // Compares group of bits to the next half of group of bits, then if it's an even amount, will eventually cancel out, if not
+  // it'll 0 out due to the XOR operator.
+  x = x ^ (x >> 16);
+  x = x ^ (x >> 8);
+  x = x ^ (x >> 4);
+  x = x ^ (x >> 2);
+  x = x ^ (x >> 1);
+  return x & 0x1; // cause of arithmetic shift
+}
+/*
+ * ilog2 - return floor(log base 2 of x), where x > 0
+ *   Example: ilog2(16) = 4
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 90
+ *   Rating: 4
+ * 
+ * 0001 0000
+ */
+int ilog2(int x) {
+  int count = 0x0;
+  count = !!(x >> 16) << 4;
+  count += !!(x >> (8 + count)) << 3;
+  count += !!(x >> (4 + count)) << 2;
+  count += !!(x >> (2 + count)) << 1;
+  count += !!(x >> (1 + count)); 
+  return count;
+}
+
+/*
+count += !(!x);
+  count += !(!(x>>1));
+  count += !(!(x>>2));
+  count += !(!(x>>3));
+  count += !(!(x>>4));
+  count += !(!(x>>5));
+  count += !(!(x>>6));
+  count += !(!(x>>7));
+  count += !(!(x>>8));
+  count += !(!(x>>9));
+  count += !(!(x>>10));
+  count += !(!(x>>11));
+  count += !(!(x>>12));
+  count += !(!(x>>13));
+  count += !(!(x>>14));
+  count += !(!(x>>15));
+  count += !(!(x>>16));
+  count += !(!(x>>17));
+  count += !(!(x>>18));
+  count += !(!(x>>19));
+  count += !(!(x>>20));
+  count += !(!(x>>21));
+  count += !(!(x>>22));
+  count += !(!(x>>23));
+  count += !(!(x>>24));
+  count += !(!(x>>25));
+  count += !(!(x>>26));
+  count += !(!(x>>27));
+  count += !(!(x>>28));
+  count += !(!(x>>29));
+  count += !(!(x>>30));
+*/
+/*
+ * trueThreeFourths - multiplies by 3/4 rounding toward 0,
+ *   avoiding errors due to overflow
+ *   Examples: trueThreeFourths(11) = 8
+ *             trueThreeFourths(-9) = -6
+ *             trueThreeFourths(1073741824) = 805306368 (no overflow)
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 20
+ *   Rating: 4
+ */
+int trueThreeFourths(int x)
+{
+  int sign = x >> 31;
+  int remainder = x & 3;
+  x = x >> 2;
+  x = x + x + x;
+  return x + (((remainder + remainder + remainder) + (sign & 3)) >> 2);
+}
+/*
+ * Extra credit
+ */
+/* 
+ * float_neg - Return bit-level equivalent of expression -f for
+ *   floating point argument f.
+ *   Both the argument and result are passed as unsigned int's, but
+ *   they are to be interpreted as the bit-level representations of
+ *   single-precision floating point values.
+ *   When argument is NaN, return argument.
+ *   Legal ops: Any integer/unsigned operations incl. ||, &&. also if, while
+ *   Max ops: 10
+ *   Rating: 2
+ */
+unsigned float_neg(unsigned uf) {
+ return 2;
+}
+/* 
+ * float_i2f - Return bit-level equivalent of expression (float) x
+ *   Result is returned as unsigned int, but
+ *   it is to be interpreted as the bit-level representation of a
+ *   single-precision floating point values.
+ *   Legal ops: Any integer/unsigned operations incl. ||, &&. also if, while
+ *   Max ops: 30
+ *   Rating: 4
+ */
+unsigned float_i2f(int x) {
+  return 2;
+}
+/* 
+ * float_twice - Return bit-level equivalent of expression 2*f for
+ *   floating point argument f.
+ *   Both the argument and result are passed as unsigned int's, but
+ *   they are to be interpreted as the bit-level representation of
+ *   single-precision floating point values.
+ *   When argument is NaN, return argument
+ *   Legal ops: Any integer/unsigned operations incl. ||, &&. also if, while
+ *   Max ops: 30
+ *   Rating: 4
+ */
+unsigned float_twice(unsigned uf) {
+  return 2;
+}
